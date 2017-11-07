963b18eccd4af4aad2a209153a0382b36deeb36f
galinam <galina.mihaleva@hotmail.com>
2017-11-07 15:22:03 +0200

    solved task

---
+++ Workshop-Arrays Methods Objects/Task 1. Messages/Program.cs
@@ -0,0 +1,86 @@
+﻿/* Telerik Academy Season 2016-2017 / C# Advanced Exam - 31 May 2016 / Task1.Messages
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+class Program
+{
+    static BigInteger ConvertToNumber(string str, string[] digitGeorgeTheGreat)
+    {
+        BigInteger result = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.Length - 2; i+=3)
+        {
+            string strDigit = str.Substring(i, 3);
+            for (int j = 0; j < 10; j++)
+            {
+                if (strDigit == digitGeorgeTheGreat[j])
+                {
+                    sb.Append(j);
+                }
+            }
+        }
+
+        string sbStr = sb.ToString();
+        result = BigInteger.Parse(sbStr);
+        return result;
+    }
+
+    static string ConvertToGeorgeTheGreat(BigInteger number, string[] digitGeorgeTheGreat)
+    {
+        string result = "";
+        StringBuilder sb = new StringBuilder();
+        string[] code = { "0","1","2","3","4","5","6","7","8","9"};
+        string digits = number.ToString();
+        for (int i = 0; i < digits.Length; i++)
+        {
+            for (int j = 0; j < code.Length; j++)
+            {
+                if (digits[i].ToString() == code[j])
+                {
+                    sb.Append(digitGeorgeTheGreat[j]);
+                }
+            }
+        }
+
+        result = sb.ToString();
+        return result;
+    }
+    static void Main()
+    {
+        string firstStr = Console.ReadLine();
+        char sign = char.Parse(Console.ReadLine());
+        string secondStr = Console.ReadLine();
+
+        string[] digitGeorgeTheGreat = 
+        {
+            "cad", 
+            "xoz",
+            "nop",
+            "cyk",
+            "min",
+            "mar",
+            "kon",
+            "iva",
+            "ogi",
+            "yan"
+        };
+
+        BigInteger result = 0;
+        if (sign == '-')
+        {
+            result = ConvertToNumber(firstStr, digitGeorgeTheGreat) - ConvertToNumber(secondStr, digitGeorgeTheGreat);
+        }
+        else if (sign == '+')
+        {
+            result = ConvertToNumber(firstStr, digitGeorgeTheGreat) + ConvertToNumber(secondStr, digitGeorgeTheGreat);
+        }
+
+        Console.WriteLine(ConvertToGeorgeTheGreat(result, digitGeorgeTheGreat));
+
+    }
+}
---
+++ Workshop-Arrays Methods Objects/Task 3. Bit Shift Matrix/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+class Program
+{
+    static int rows;
+    static int cols;
+    static int[] moves;
+    static BigInteger[,] field;
+
+    static void Main()
+    {
+        rows = int.Parse(Console.ReadLine());
+        cols = int.Parse(Console.ReadLine());
+        int movesCount = int.Parse(Console.ReadLine());
+        moves = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        field = new BigInteger[rows, cols];
+        FillMatrix();
+        PrintMatrix(field);
+
+        //logic
+        BigInteger sum = 0;
+        int[] pos = { rows - 1, 0 };
+        int coef = rows > cols ? rows : cols;
+
+        sum += field[pos[0], pos[1]];
+        field[pos[0], pos[1]] = 0;
+
+        foreach (var code in moves)
+        {
+            int[] targetPos = { code / coef, code % coef };
+
+            int stepRow = pos[0] == targetPos[0] ? 0 : (pos[0] < targetPos[0] ? 1 : -1);    // int stepRow = Math.Sign(targetPos[0] - pos[0]);
+            int stepCol = pos[1] == targetPos[1] ? 0 : (pos[1] < targetPos[1] ? 1 : -1);    // int stepCol = Math.Sign(targetPos[1] - pos[1]); 
+
+            do
+            {
+                pos[1] += stepCol;
+                sum += field[pos[0], pos[1]];
+                field[pos[0], pos[1]] = 0;
+            } while (pos[1] != targetPos[1] && pos[1] >= 0 && pos[1] < cols);
+
+            do
+            {
+                pos[0] += stepRow;
+                sum += field[pos[0], pos[1]];
+                field[pos[0], pos[1]] = 0;
+            } while (pos[0] != targetPos[0] && pos[0] >= 0 && pos[0] < rows);
+            
+        }
+
+        Console.WriteLine(sum);
+    }
+    private static void FillMatrix()
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                field[r, c] = (BigInteger)(1) << (rows - 1 - r + c);
+            }
+        }
+    }
+    private static void PrintMatrix(BigInteger[,] board)
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                Console.Write("{0,4}", board[r, c]);
+            }
+            Console.WriteLine();
+        }
+    }    
+}
